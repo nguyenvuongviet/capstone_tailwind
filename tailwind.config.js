496323aa2d92862834e09d8594f198da1cca8062
nguyenvuongviet <nguyenvuongviet2k4@gmail.com>
2024-11-02 02:28:13 +0700

    final code

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,22 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        'xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+        xl: "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
       },
-
+      inset: {
+        "10%": "10%",
+        "20%": "20%",
+        "5%": "5%",
+        "-4rem": "-4rem",
+      },
+      width: {
+        "120%": "120%",
+      },
+      animation: {
+        spin: "spin 20s linear infinite",
+      },
     },
   },
-  darkMode: 'class',
+  darkMode: "class",
   plugins: [],
-}
+};
